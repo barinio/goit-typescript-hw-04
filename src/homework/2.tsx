7fd50c5a6acbc303bfa00fe7303a64d76daf4a73
Ваня <7328652@gmail.com>
2023-11-22 01:10:54 +0200

    change type Action

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -5,9 +5,12 @@ type State = {
 	requestStep: string;
 }
 
-type Action = {
-	type: 'START_REQUEST'|'PENDING_REQUEST'|'FINISH_REQUEST'|'RESET_REQUEST';
-} 
+type Action =
+	|{type: 'START_REQUEST'}
+	|{type: 'PENDING_REQUEST'}
+	|{type: 'FINISH_REQUEST'}
+	|{type: 'RESET_REQUEST'}
+
 
 const initialState: State = {
   isRequestInProgress: false,
