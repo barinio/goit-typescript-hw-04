3c7925d216298b8f42dbd8c578119d8fdeda8702
Ваня <7328652@gmail.com>
2023-11-21 21:56:51 +0200

    first commit

---
+++ src/homework/3.tsx
@@ -0,0 +1,13 @@
+import  { ChangeEvent, useState } from 'react';
+
+export function FormComponent() {
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <input type="text" value={value} onChange={handleChange} />
+  );
+}
